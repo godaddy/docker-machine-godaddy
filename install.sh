5fb33d7a3fe1971eba7b66fd9845efd3166e0fbe
Bo Thompson <ethompson@godaddy.com>
2016-08-06 03:05:57 +0000

    fix target install dir

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+set -e
+
 VERSION=v1.0.0
 REPO=https://github.com/aka-bo/docker-machine-godaddy
 
+echo "Downloading driver..."
 curl -L ${REPO}/releases/download/${VERSION}/docker-machine-driver-godaddy-$(uname -s) > docker-machine-driver-godaddy
+
+echo "Installing..."
 chmod +x docker-machine-driver-godaddy
-mv docker-machine-driver-godaddy /usr/local/lib/
+mv docker-machine-driver-godaddy /usr/local/bin/
+
+echo "Install complete."
+exit 0
